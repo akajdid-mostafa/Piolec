f93efd3ec48d318a04d5d80eaa4c6395a4863853
mostafa <mostafaakajdid6@gmail.com>
2025-01-11 20:24:13 +0100

    mos

--- app/etude-et-installation-electrique-complete/page.js
+++ app/etude-et-installation-electrique-complete/page.js
@@ -5,17 +5,211 @@ import { GrowBusiness2 } from "@/components/GrowBusiness";
 import Pricing from "@/components/Pricing";
 import WorkingProcess from "@/components/WorkingProcess";
 import NextLayout from "@/layouts/NextLayout";
-// import Link from "next/link";
-import { Start,Start2 } from "@/components/servicess";
+import Link from "next/link";
+import * as servicess from "@/components/servicess";
 
 const page = () => {
   return (
     <NextLayout>
       {/* <Breadcrumb pageName="Étude et installation électrique complète." /> */}
       {/* About Section Start */}
-      <Start/>
+      <section className="about-section fix section-padding">
+        <div className="container">
+          <div className="about-wrapper">
+            <div className="row g-4 align-items-center justify-content-between">
+              <div className="col-lg-5">
+                <div className="about-content ms-0">
+                  <div className="section-title">
+                    <span className="sub-content wow fadeInUp">
+                      <img src="assets/img/bale.png" alt="img" />
+                      Problèmes Solutions
+                    </span>
+                    <h2 className="wow fadeInUp" data-wow-delay=".3s">
+                      Votre succès, notre électricité de confiance
+                    </h2>
+                  </div>
+                  <p className="mt-3 mt-md-0 wow fadeInUp" data-wow-delay=".5s">
+                    Chez PIOLEC, nous mettons notre expertise à votre service
+                    pour des installations électriques sûres, efficaces et
+                    conformes aux normes. Que ce soit pour un projet
+                    résidentiel, industriel ou commercial, nous vous
+                    accompagnons de l&apos;étude à la réalisation, en garantissant
+                    qualité et fiabilité à chaque étape.
+                  </p>
+                  <ul className="about-list wow fadeInUp" data-wow-delay=".3s">
+                    <li>
+                      <i className="fas fa-check" />
+                      Connectez-vous à des solutions électriques sans tracas.
+                    </li>
+                    <li>
+                      <i className="fas fa-check" />
+                      Profitez de services rapides et fiables.
+                    </li>
+                    <li>
+                      <i className="fas fa-check" />
+                      Trouvez des solutions qui répondent à vos besoins en un
+                      temps record.
+                    </li>
+                  </ul>
+                  <div
+                    className="about-button mt-5 wow fadeInUp"
+                    data-wow-delay=".5s"
+                  >
+                    <Link href="about" className="theme-btn">
+                      En savoir plus sur nous
+                      <i className="far fa-arrow-right" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="about-image-items-4">
+                  <div className="experience-items">
+                    <h6>
+                      Plus de 400 clients au Maroc et au-delà lui font
+                      confiance.
+                    </h6>
+                    <img src="assets/img/about/face-mans.png" alt="img" />
+                    <h2>
+                      <span className="count">
+                        <Counter end={5} />
+                      </span>
+                      +
+                    </h2>
+                    <p>Années d&apos;expérience</p>
+                  </div>
+                  <div className="row g-4 align-items-center">
+                    <div className="col-lg-6">
+                      <div className="row g-4">
+                        <div
+                          className="col-md-12 wow fadeInUp"
+                          data-wow-delay=".3s"
+                        >
+                          <div className="about-thumbs">
+                            <img src="assets/img/about/07.jpg" alt="img" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-12 wow fadeInUp"
+                          data-wow-delay=".5s"
+                        >
+                          <div className="about-thumbs">
+                            <img src="assets/img/about/08.jpg" alt="img" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 wow fadeInUp" data-wow-delay=".7s">
+                      <div className="about-thumbs">
+                        <img src="assets/img/about/09.jpg" alt="img" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       {/* Service Video Section Start */}
-      <Start2/>
+      <section className="about-section fix section-padding">
+        <div className="container">
+          <div className="about-wrapper">
+            <div className="row g-4 align-items-center justify-content-between">
+              <div className="col-lg-5">
+                <div className="about-content ms-0">
+                  <div className="section-title">
+                    <span className="sub-content wow fadeInUp">
+                      <img src="assets/img/bale.png" alt="img" />
+                      Problèmes Solutions
+                    </span>
+                    <h2 className="wow fadeInUp" data-wow-delay=".3s">
+                      Votre succès, notre électricité de confiance
+                    </h2>
+                  </div>
+                  <p className="mt-3 mt-md-0 wow fadeInUp" data-wow-delay=".5s">
+                    Chez PIOLEC, nous mettons notre expertise à votre service
+                    pour des installations électriques sûres, efficaces et
+                    conformes aux normes. Que ce soit pour un projet
+                    résidentiel, industriel ou commercial, nous vous
+                    accompagnons de l&apos;étude à la réalisation, en garantissant
+                    qualité et fiabilité à chaque étape.
+                  </p>
+                  <ul className="about-list wow fadeInUp" data-wow-delay=".3s">
+                    <li>
+                      <i className="fas fa-check" />
+                      Connectez-vous à des solutions électriques sans tracas.
+                    </li>
+                    <li>
+                      <i className="fas fa-check" />
+                      Profitez de services rapides et fiables.
+                    </li>
+                    <li>
+                      <i className="fas fa-check" />
+                      Trouvez des solutions qui répondent à vos besoins en un
+                      temps record.
+                    </li>
+                  </ul>
+                  <div
+                    className="about-button mt-5 wow fadeInUp"
+                    data-wow-delay=".5s"
+                  >
+                    <Link href="about" className="theme-btn">
+                      En savoir plus sur nous
+                      <i className="far fa-arrow-right" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="about-image-items-4">
+                  <div className="experience-items">
+                    <h6>
+                      Plus de 400 clients au Maroc et au-delà lui font
+                      confiance.
+                    </h6>
+                    <img src="assets/img/about/face-mans.png" alt="img" />
+                    <h2>
+                      <span className="count">
+                        <Counter end={5} />
+                      </span>
+                      +
+                    </h2>
+                    <p>Années d&apos;expérience</p>
+                  </div>
+                  <div className="row g-4 align-items-center">
+                    <div className="col-lg-6">
+                      <div className="row g-4">
+                        <div
+                          className="col-md-12 wow fadeInUp"
+                          data-wow-delay=".3s"
+                        >
+                          <div className="about-thumbs">
+                            <img src="assets/img/about/07.jpg" alt="img" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-12 wow fadeInUp"
+                          data-wow-delay=".5s"
+                        >
+                          <div className="about-thumbs">
+                            <img src="assets/img/about/08.jpg" alt="img" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 wow fadeInUp" data-wow-delay=".7s">
+                      <div className="about-thumbs">
+                        <img src="assets/img/about/09.jpg" alt="img" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       {/* Working Process Section Start */}
       <WorkingProcess
        title1="Découverte et analyse"
