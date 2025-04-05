e5db59b08f304cc2a962e5e0de2e0d28a3b4f74d
mostafa <mostafaakajdid6@gmail.com>
2025-04-05 18:22:17 +0000

    mos

--- components/contact.jsx
+++ components/contact.jsx
@@ -4,81 +4,68 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeCircleCheck, faPhoneVolume, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
-const socialLinks = [
-  {
-    imgSrc: "/images/Footer/facebook.svg",
-    link: "https://www.facebook.com/profile.php?id=61567020357925&mibextid=ZbWKwL",
-    width: 20,
-  },
-  {
-    imgSrc: "/images/Footer/insta.svg",
-    link: "https://www.instagram.com/ocean.connecting",
-    width: 28,
-  },
-  { imgSrc: "/images/Footer/twitter.svg", link: "#", width: 28 },
-  {
-    imgSrc: "/images/Footer/youtube.svg",
-    link: "https://www.youtube.com/results?search_query=ocean+connecting",
-    width: 28,
-  },
-];
-
 const Contact = () => {
   return (
     <div id="Contact" className="contact-container">
       <div className="contact-inner-container">
-        <div className="contact-content wow fadeInUp"
-        data-wow-delay=".4s">
+        <div className="contact-content wow fadeInUp" data-wow-delay=".4s">
           <div className="contact-grid">
-            <ContactCard
-              icon={<FontAwesomeIcon icon={faEnvelopeCircleCheck} className="contact-card-icon" />}
-              title="Email"
-              description="Contactez-nous à l'adresse suivante"
-              link="mailto:piolec21@outlook.com"
-              linkText="piolec21@outlook.com"
-              linkTextt=""
-              
-            />
-            <ContactCard
-              icon={<FontAwesomeIcon icon={faPhoneVolume} className="contact-card-icon" />}
-              title="Téléphone"
-              description="Nous Contacter par Téléphone"
-              link="tel:+212662-241871"
-              linkText="+212 662-241871"
-              linkTextt="+212 666491605"
-            />
-            <ContactCard
-              icon={<FontAwesomeIcon icon={faLocationDot} className="contact-card-icon" />}
-              title="Emplacement"
-              description="Nous Trouver dans Nos Centres"
-              link="https://goo.gl/maps/QcWzYETAh4FS3KTD7"
-              linkText="61 AV. Lalla yacout, N°39, 1er étage centre riad, casablanca"
-              linkTextt=""
-            />
+            
+            {/* Email Card */}
+            <div className="contact-card">
+              <span className="contact-card-icon">
+                <FontAwesomeIcon icon={faEnvelopeCircleCheck} className="contact-card-icon" />
+                <p className="contact-card-title">Email</p>
+              </span>
+              <p className="contact-card-description">Contactez-nous à l'adresse suivante</p>
+              <a href="mailto:piolec21@outlook.com" className="contact-card-link">
+                piolec21@outlook.com
+              </a>
+            </div>
+
+            {/* Phone Card */}
+            <div className="contact-card">
+              <span className="contact-card-icon">
+                <FontAwesomeIcon icon={faPhoneVolume} className="contact-card-icon" />
+                <p className="contact-card-title">Téléphone</p>
+              </span>
+              <p className="contact-card-description">Nous Contacter par Téléphone</p>
+              <a href="tel:+212662-241871" className="contact-card-link">
+                +212 662-241871<br />
+                +212 666491605
+              </a>
+            </div>
+
+            {/* Location Card */}
+            <div className="contact-card">
+              <span className="contact-card-icon">
+                <FontAwesomeIcon icon={faLocationDot} className="contact-card-icon" />
+                <p className="contact-card-title">Emplacement</p>
+              </span>
+              <p className="contact-card-description">Nous Trouver dans Nos Centres</p>
+              <a 
+                href="https://goo.gl/maps/QcWzYETAh4FS3KTD7" 
+                className="contact-card-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>Siège : </span> Lalla yacout, N°39, 1er étage, casablanca
+              </a>
+              <a 
+                href="https://goo.gl/maps/QcWzYETAh4FS3KTD7" 
+                className="contact-card-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>succursale : </span> Agadir
+              </a>
+            </div>
+
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-const ContactCard = ({ icon, title, description, link, linkText , linkTextt }) => (
-  <div className="contact-card">
-    <span className="contact-card-icon">
-      {icon}
-      <p className="contact-card-title">{title}</p>
-    </span>
-    <p className="contact-card-description">{description}</p>
-    <a href={link} className="contact-card-link">
-      {linkText}
-      <br></br>
-      {linkTextt}
-    </a>
-  </div>
-);
-
-const InputField = ({ placeholder }) => (
-  <input className="input-field" placeholder={placeholder} />
-);
 
 export default Contact;
