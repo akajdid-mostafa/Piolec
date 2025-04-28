ee1aae044f54a1386ff71f4e724ff06ad9a632c7
mostafa <mostafaakajdid6@gmail.com>
2025-04-28 12:46:10 +0100

    mos

--- components/mark.jsx
+++ components/mark.jsx
@@ -4,12 +4,26 @@ import React from "react";
 import Slider from "react-slick";
 
 const data = [
-  { imgSrc: "/assets/img/mark/01.png" },
-  { imgSrc: "/assets/img/mark/02.png" },
-  { imgSrc: "/assets/img/mark/03.png" },
-  { imgSrc: "/assets/img/mark/04.png" },
-  { imgSrc: "/assets/img/mark/05.png" },
-  { imgSrc: "/assets/img/mark/06.png" },
+  { imgSrc: "/assets/img/mark/schnieder.svg" },
+  { imgSrc: "/assets/img/mark/siemens.svg" },
+  { imgSrc: "/assets/img/mark/ingelec.svg" },
+  { imgSrc: "/assets/img/mark/nexans.svg" },
+  { imgSrc: "/assets/img/mark/abb.svg" },
+  { imgSrc: "/assets/img/mark/eaton.svg" },
+  { imgSrc: "/assets/img/mark/legrand.svg" },
+  { imgSrc: "/assets/img/mark/jm-concept.svg" },
+  { imgSrc: "/assets/img/mark/victron-energy.svg" },
+  { imgSrc: "/assets/img/mark/philips.svg" },
+  { imgSrc: "/assets/img/mark/tosun.svg" },
+  { imgSrc: "/assets/img/mark/esser.svg" },
+  { imgSrc: "/assets/img/mark/unv.svg" },
+  { imgSrc: "/assets/img/mark/hik-vision.svg" },
+  { imgSrc: "/assets/img/mark/ruijie.svg" },
+  { imgSrc: "/assets/img/mark/suprema.svg" },
+  { imgSrc: "/assets/img/mark/tdsi.svg" },
+  { imgSrc: "/assets/img/mark/anviz.svg" },
+  { imgSrc: "/assets/img/mark/teletek.svg" },
+  { imgSrc: "/assets/img/mark/tecno-alarm.svg" },
 ];
 
 export default function Mark() {
@@ -60,7 +74,7 @@ export default function Mark() {
       <div className="section-title text-center">
           <span className="sub-content wow fadeInUp">
             {/* <img src="assets/img/bale.png" alt="img" /> */}
-            Marques partenaires
+            Nos partenaires
           </span>
           {/* <h2 className="wow fadeInUp" data-wow-delay=".3s">
           Questions fréquemment posées
